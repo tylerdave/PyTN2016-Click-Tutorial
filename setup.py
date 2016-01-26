809bbf5d9096ba956a68edf6c4f25aaab8da10f4
Dave Forgac <dforgac@ag.com>
2016-01-26 15:29:34 -0500

    require tox

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 test_requirements = [
-    'setuptools>17.1',
     'mock',
     'pytest',
+    'tox',
 ]
 
 requirements = [
