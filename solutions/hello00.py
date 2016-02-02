e5b25cce57633336dbea06ae1256f4123fecb440
Dave Forgac <tylerdave@tylerdave.com>
2016-02-01 22:20:44 -0500

    remove old solutions

--- solutions/hello00.py
+++
@@ -1,8 +0,0 @@
-import click
-
-@click.command()
-def cli():
-    click.echo("Hello, World!")
-
-if __name__ == '__main__':
-    cli()
--- solutions/hello01.py
+++
@@ -1,9 +0,0 @@
-import click
-
-@click.command()
-@click.argument('name')
-def cli(name):
-    click.echo("Hello, {0}!".format(name))
-
-if __name__ == '__main__':
-    cli()
