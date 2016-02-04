46610157d626de457ff940d18801b2bcda1106c0
Dave Forgac <tylerdave@tylerdave.com>
2016-02-04 09:04:47 -0500

    fix hello world test class name

--- tutorial/hello_world.py
+++ tutorial/hello_world.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from .base import BaseTutorialLesson
 
-class TestTutorialBasicArguments(BaseTutorialLesson):
+class TestTutorialHelloWorld(BaseTutorialLesson):
 
-    def test_00_cli_with_single_argument(self):
+    def test_00_cli_outputs_hello_message(self):
         result = self.run_command()
         assert result.output == 'Hello, PyTN!\n'
