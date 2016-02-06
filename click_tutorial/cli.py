23ec488562e2f4b1ae35f16d31073e586d6a8e97
Dave Forgac <tylerdave@tylerdave.com>
2016-02-06 12:37:27 -0600

    revert accidental changes to cli

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,22 +1,11 @@
 import click
 
-@click.group(name='pytn')
-@click.option('--input-string1', help='Optional input string.')
-@click.option('--input-string2', metavar='<string>', help='Another optional input string.')
-def cli(do_stuff, input_string1, input_string2):
+@click.command(name='pytn')
+def cli():
     """
-    Outputs messages.
-    """
-
-@cli.command()
-def hello():
-    """
-    Says hello.
+    Output a greeting to PyTennessee!
     """
     click.echo("Hello, PyTN!")
-
-
-
 
 if __name__ == '__main__':
     cli()
