57502a5ae55ccfe526370b29148b15293c60522c
Dave Forgac <tylerdave@tylerdave.com>
2016-02-07 02:07:09 -0600

    add file lesson

--- solutions/file_types.py
+++ solutions/file_types.py
@@ -1,11 +1,15 @@
 import click
 
 @click.command(name='pytn')
-def cli():
+@click.argument('infile', type=click.File('r'), default='-')
+@click.argument('outfile', type=click.File('w'), default='-')
+def cli(infile, outfile):
     """
-    Validates input types.
+    Reads / writes files.
     """
-    click.echo("Hello, PyTN!")
+    text = infile.read()
+    click.echo("Input chars: {0}".format(len(text)), err=True)
+    click.secho(text, file=outfile, fg='green')
 
 if __name__ == '__main__':
     cli()
--- tutorial/file_types.py
+++ tutorial/file_types.py
@@ -5,7 +5,28 @@ class TestFileTypes(BaseTutorialLesson):
 
     # TODO: add tests
 
-    def test_00_cli(self):
-        result = self.run_command()
-        assert True
 
+    def test_00_cli_file_input_stdout(self):
+        with self.runner.isolated_filesystem():
+            with open('infile.txt', 'w') as f:
+                f.write('Input data.')
+            result = self.run_command(['infile.txt'])
+            assert 'Input data.\n' in result.output
+
+    def test_01_cli_file_input_file_output(self):
+        with self.runner.isolated_filesystem():
+            with open('infile.txt', 'w') as f:
+                f.write('Input data.')
+            result = self.run_command(['infile.txt', 'outfile.txt'])
+            with open('outfile.txt', 'r') as f:
+                data = f.read()
+            assert data == 'Input data.\n'
+            assert 'Input data.' not in result.output
+
+    def test_02_cli_stdin_file_output(self):
+        with self.runner.isolated_filesystem():
+            result = self.run_command(['-', 'outfile.txt'], input="Input data.")
+            with open('outfile.txt', 'r') as f:
+                data = f.read()
+            assert data == 'Input data.\n'
+            assert 'Input data.' not in result.output
