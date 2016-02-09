eaa5e0a40e93c213536eae2c8fc79770f4d1d778
Dave Forgac <tylerdave@tylerdave.com>
2016-02-09 10:48:08 -0500

    remove unused kwarg

--- solutions/basic_usage_documentation.py
+++ solutions/basic_usage_documentation.py
@@ -3,7 +3,7 @@ import click
 @click.group(name='pytn')
 @click.option('--input-string1', help='Optional input string.')
 @click.option('--input-string2', metavar='<string>', help='Another optional input string.')
-def cli(do_stuff, input_string1, input_string2):
+def cli(input_string1, input_string2):
     """
     Outputs messages.
     """
